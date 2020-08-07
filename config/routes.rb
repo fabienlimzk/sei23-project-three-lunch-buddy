838d610473311980f7882a461be8198b077fff55
Joey <61016347+joeyqlim@users.noreply.github.com>
2020-08-07 16:47:08 +0800

    Revert "added authentication with devise and basic user MVC with flash msg"

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'users/index'
-  get 'users/show'
-  get 'users/destroy'
-  get 'users/update'
-  get 'users/edit'
-  devise_for :users
-
   get 'appointments/index'
   get 'appointments/show'
   get 'appointments/new'
@@ -14,6 +6,5 @@ Rails.application.routes.draw do
   get 'appointments/edit'
   get 'appointments/update'
   get 'appointments/destroy'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
