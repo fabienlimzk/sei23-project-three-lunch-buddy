1efe76941b4fc1d3b817a90850f4aec3baad56e7
joeyqlim <joeyqlim@gmail.com>
2020-08-07 17:18:33 +0800

    add navbar and routing for appt and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-
-  get 'users/index'
-  get 'users/show'
-  get 'users/destroy'
-  get 'users/update'
-  get 'users/edit'
   devise_for :users
 
-  get 'appointments/index'
-  get 'appointments/show'
-  get 'appointments/new'
-  get 'appointments/create'
-  get 'appointments/edit'
-  get 'appointments/update'
-  get 'appointments/destroy'
+  resources :appointments
+  
+  resources :users do
+    resources :appointments
+  end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root 'appointments#index'
 end
