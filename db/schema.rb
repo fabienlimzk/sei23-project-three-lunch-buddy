d854786b8ba7fb8c8ffa9fbc3dac579247cf392a
akjw <64943623+akjw@users.noreply.github.com>
2020-08-10 10:55:09 +0800

    Revert "add enum to appointment status"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_191926) do
+ActiveRecord::Schema.define(version: 2020_08_08_045244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,10 @@ ActiveRecord::Schema.define(version: 2020_08_08_191926) do
 
   create_table "appointments", force: :cascade do |t|
     t.string "content"
+    t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
-    t.integer "status"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
