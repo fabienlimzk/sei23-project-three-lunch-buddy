438467b56585c2cd4cb22c6cc3d9a8e920f73c70
akjw <aseykoh@gmail.com>
2020-08-10 15:46:57 +0800

    update seed file for tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create(name: 'CBD')
+Location.create(name: 'West')
+Location.create(name: 'East')
+Location.create(name: 'Northeast')
+
+Cuisine.create(name: 'Mexican')
+Cuisine.create(name: 'Halal')
+
+Price.create(name: '<$5')
+Price.create(name: '<$10')
+Price.create(name: '<$20')
+Price.create(name: '<$30')
+Price.create(name: '<$40')
+Price.create(name: '<$50')
+Price.create(name: '$50++')
