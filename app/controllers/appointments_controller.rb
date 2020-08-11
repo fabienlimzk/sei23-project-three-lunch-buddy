30b8e20dc97fd080f0671bafbbcd67284dfc9eef
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-08-11 13:46:07 +0800

    edit and delete appt

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -26,12 +26,26 @@ class AppointmentsController < ApplicationController
   end
 
   def edit
+    @user = User.find(current_user.id)
+    @appointment = Appointment.find(params[:id])
   end
 
   def update
+    @user = User.find(current_user.id)
+    @appointment = Appointment.find(params[:id])
+    
+    if @appointment.update(appointment_params)
+      redirect_to user_appointments_path(current_user.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @appointment = Appointment.find(params[:id]).destroy   
+    if @appointment.destroy
+      redirect_to user_appointments_path(current_user.id)
+    end
   end
 
   private 
