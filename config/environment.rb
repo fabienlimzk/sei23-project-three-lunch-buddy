5ef384df252090a524559052886f3f2e814824ea
Fabien Lim Z K <63240153+fabienlimzk@users.noreply.github.com>
2020-08-12 20:26:51 +0800

    Revert "Revert "Add appt upcoming property ""

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ # SMTP settings for gmail
+ ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['gmail_username'],
+  :password             => ENV['gmail_password'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
