77464e4eb189d6969c12855ff38c2548d3205096
joeyqlim <joeyqlim@gmail.com>
2020-08-12 16:32:28 +0800

    fix navbar on very large screens, fix flash padding, fix devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   get 'homepage/index'
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'appointments#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'homepage#index', as: :unauthenticated_root
+    end
+  end
 
   put '/appointments/:id/accept', to: 'appointments#accept', as: "accept"
 
@@ -16,6 +25,4 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root 'homepage#index'
-
 end
